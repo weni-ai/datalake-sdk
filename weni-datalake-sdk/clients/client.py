b1f9822d4d5bae71f76067a63ed0f740cd6d9f4c
lucaslinhares <lucaslinhares027@gmail.com>
2025-03-24 17:09:30 -0300

    Update lib version to 0.0.3

--- weni-datalake-sdk/clients/client.py
+++ weni-datalake-sdk/clients/client.py
@@ -4,17 +4,14 @@ import msgs_pb2_grpc
 
 SERVER_ADDRESS = "localhost:50051"
 
-def send_data():
+def send_data(path, data):
     channel = grpc.insecure_channel(SERVER_ADDRESS)
     stub = msgs_pb2_grpc.DatalakeManagerServiceStub(channel)
 
     request = msgs_pb2.InsertRequest(
-        path="message",
-        data={"project_id": "100", "text": "Agora vai meu amigo", "direction": "O"}
+        path=path,
+        data=data
     )
 
     response = stub.InsertData(request)
     print("Server response:", response.status)
-
-if __name__ == "__main__":
-    send_data()
