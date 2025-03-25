f4da7ff21116d9511abf0fb9e27df5543ff1d6fa
lucaslinhares <lucaslinhares027@gmail.com>
2025-03-25 17:00:36 -0300

    Fix path validation

--- weni_datalake_sdk/clients/client.py
+++ weni_datalake_sdk/clients/client.py
@@ -1,5 +1,6 @@
 import grpc
 from weni_datalake_sdk.clients import msgs_pb2, msgs_pb2_grpc
+from weni_datalake_sdk.paths.validator import validate_path
 
 SERVER_ADDRESS = "localhost:50051"
 
@@ -7,8 +8,13 @@ def send_data(path, data):
     channel = grpc.insecure_channel(SERVER_ADDRESS)
     stub = msgs_pb2_grpc.DatalakeManagerServiceStub(channel)
 
+    if isinstance(path, type):
+        path = path()
+
+    validate_path(path)
+
     request = msgs_pb2.InsertRequest(
-        path=path,
+        path=path.get_table_name(),
         data=data
     )
 
