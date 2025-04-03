fc8c7a3ff273fabf2b9c4c03b520bd7ade455551
lucaslinhares <lucaslinhares027@gmail.com>
2025-04-03 00:19:05 -0300

    Add trace path

--- weni_datalake_sdk/clients/client.py
+++ weni_datalake_sdk/clients/client.py
@@ -1,10 +1,18 @@
 import grpc
-from django.conf import settings
 
-from weni_datalake_sdk.clients import msgs_pb2, msgs_pb2_grpc
+from weni_datalake_sdk.clients import (
+    msgs_pb2,
+    msgs_pb2_grpc,
+    traces_pb2,
+    traces_pb2_grpc,
+)
 from weni_datalake_sdk.paths.validator import validate_path
 
-SERVER_ADDRESS = settings.DATALAKE_SERVER_ADDRESS
+# from django.conf import settings
+
+
+# SERVER_ADDRESS = settings.DATALAKE_SERVER_ADDRESS
+SERVER_ADDRESS = "localhost:50051"  # Replace with your actual server address
 
 
 def send_data(path, data):
@@ -20,3 +28,15 @@ def send_data(path, data):
 
     response = stub.InsertData(request)
     print("Server response:", response.status)
+
+
+def send_trace_data(path_class, data):
+    validate_path(path_class)
+
+    channel = grpc.insecure_channel(SERVER_ADDRESS)
+    stub = traces_pb2_grpc.DatalakeManagerServiceStub(channel)
+
+    request = traces_pb2.InsertTraceRequest(path=path_class.get_table_name(), data=data)
+
+    response = stub.InsertTraceData(request)
+    return response.status
