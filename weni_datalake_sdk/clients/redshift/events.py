ea570c8ecb1aac562d07db939ae3276d922360c0
lucaslinhares <lucaslinhares027@gmail.com>
2025-07-16 17:21:36 -0300

    Add get_events_count and clean quotes function

--- weni_datalake_sdk/clients/redshift/events.py
+++ weni_datalake_sdk/clients/redshift/events.py
@@ -3,8 +3,24 @@ import os
 from weni_datalake_sdk.clients.redshift.redshift_client import query_dc_api
 
 
+def clean_quotes(obj):
+    if isinstance(obj, dict):
+        return {k: clean_quotes(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [clean_quotes(item) for item in obj]
+    elif isinstance(obj, str):
+        # Remove aspas duplas no início e fim, se existirem
+        if obj.startswith('"') and obj.endswith('"'):
+            return obj[1:-1]
+        return obj
+    else:
+        return obj
+
+
 def get_events(**kwargs) -> dict:
     metric = os.environ.get("EVENTS_METRIC_NAME")
+    metric = "weni-events-stg"
+    print("metric: ", metric)
 
     if not kwargs.get("project"):
         raise Exception("Project is required")
@@ -17,7 +33,31 @@ def get_events(**kwargs) -> dict:
 
     try:
         result = query_dc_api(metric=metric, query_params=kwargs)
-        return result.json()
+        data = result.json()
+        return clean_quotes(data)
 
     except Exception as e:
         raise Exception(f"Error querying events: {e}")
+
+
+def get_events_count(**kwargs) -> dict:
+    #metric = os.environ.get("EVENTS_COUNT_METRIC_NAME")
+    metric = "weni-events-count"
+    print("metric: ", metric)
+
+    if not kwargs.get("project"):
+        raise Exception("Project is required")
+
+    if not kwargs.get("date_start"):
+        raise Exception("Date start is required")
+
+    if not kwargs.get("date_end"):
+        raise Exception("Date end is required")
+
+    try:
+        result = query_dc_api(metric=metric, query_params=kwargs)
+        data = result.json()
+        return data
+
+    except Exception as e:
+        raise Exception(f"Error querying events count: {e}")
